bdd1c2e77b56034f45a7c9649d4a9687c9191716
Nick Spencer <strangebrew202@gmail.com>
2023-08-25 15:30:33 -0600

    Add: feature test for alphabetical sort link in /breweries/:id/beers

---
+++ spec/features/breweries/show_beers_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "Show Breweries Beers" do
+  describe "As a user" do
+    describe "when I visit /breweries/:id/beers" do
+      it "displays the beers for the brewery in alphabetical order" do
+        @brewer = Brewery.create(name: "Weld Works", location: "Greeley", total_taps: 25, allow_dogs: true)
+        @beer_1 = @brewer.beers.create(style: 'Lager', on_tap: true, pint_price: 6.00, name: 'Willy')
+        @beer_2 = @brewer.beers.create(style: 'Lager', on_tap: true, pint_price: 6.00, name: 'Clark')
+        @beer_3 = @brewer.beers.create(style: 'Lager', on_tap: true, pint_price: 6.00, name: 'Anderson Ale')
+
+        visit "/breweries/#{@brewer.id}/beers"
+
+        expect(@beer_1.name).to appear_before(@beer_3.name)
+        expect(@beer_1.name).to appear_before(@beer_2.name)
+        expect(@beer_2.name).to appear_before(@beer_3.name)
+
+        click_link(href: "/breweries/#{@brewer.id}/beers?sorted=true")
+
+        expect(@beer_3.name).to appear_before(@beer_2.name)
+        expect(@beer_3.name).to appear_before(@beer_1.name)
+        expect(@beer_2.name).to appear_before(@beer_1.name)
+      end
+    end
+  end
+end
