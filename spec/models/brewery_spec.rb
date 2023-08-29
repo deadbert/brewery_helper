032f3e20709a99f48c84407c154b037e90e6b5a5
Nick Spencer <strangebrew202@gmail.com>
2023-08-29 12:42:19 -0600

    Add: #beers_count model test

--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe Brewery, type: :model do
     it { should allow_value(true).for(:allow_dogs) } 
     it { should allow_value(false).for(:allow_dogs) }
   end
+
+  describe "#beers_count" do
+    it "returns a count of the beers a brewery has" do
+      brewery_1 = Brewery.create(name: "Weld Works", location: "Greeley", total_taps: 25, allow_dogs: true)
+      beer_1 = brewery_1.beers.create(style: 'IPA', on_tap: true, pint_price: 7.50, name: "Space Ghost")
+      beer_2 = brewery_1.beers.create(style: 'IPA', on_tap: true, pint_price: 7.50, name: "Juicy Bits")
+      beer_3 = brewery_1.beers.create(style: 'IPA', on_tap: false, pint_price: 6.50, name: "Willy")
+
+      expect(brewery_1.beers_count).to eq(3)
+    end
+  end
 end
