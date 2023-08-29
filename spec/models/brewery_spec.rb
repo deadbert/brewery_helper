1f660bf843bdfa8a89755ccb09052b00519eb6ec
Nick Spencer <strangebrew202@gmail.com>
2023-08-29 13:15:37 -0600

    Add: Model test for #order_by_total_beers

--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -43,4 +43,23 @@ RSpec.describe Brewery, type: :model do
       expect(brewery_1.cost_more_than(7.00)).to eq([beer_1, beer_2])
     end
   end
+
+  describe "order_by_total_beers" do
+    it "returns all breweries ordered by highest count of beers" do
+      brewery_1 = Brewery.create(name: "Weld Works", location: "Greeley", total_taps: 25, allow_dogs: true)
+      brewery_2 = Brewery.create(name: "New Belgium", location: "Fort Collins", total_taps: 18, allow_dogs: true)
+      brewery_3 = Brewery.create(name: "Zwei", location: "Fort Collins", total_taps: 12, allow_dogs: true)
+
+      brewery_1.beers.create(style: 'IPA', on_tap: true, pint_price: 7.50, name: "Space Ghost")
+      brewery_1.beers.create(style: 'IPA', on_tap: true, pint_price: 7.50, name: "Juicy Bits")
+      brewery_1.beers.create(style: 'IPA', on_tap: false, pint_price: 6.50, name: "Willy")
+
+      brewery_2.beers.create(style: 'Amber', on_tap: true, pint_price: 6.50, name: "Fat Tire")
+      brewery_2.beers.create(style: 'IPA', on_tap: true, pint_price: 7.50, name: "Voodo Ranger")
+
+      brewery_3.beers.create(style: 'Dunkel', on_tap: true, pint_price: 6.50, name: "Dunkel")
+      
+      expect(Brewery.order_by_total_beers).to eq([brewery_1, brewery_2, brewery_3])
+    end
+  end
 end
