ff551a786fe7c6d7e2aac6486db04e7169d60ec6
Nick Spencer <strangebrew202@gmail.com>
2023-08-29 12:40:06 -0600

    Add: Model test for #beers_on_tap

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe Beer, type: :model do
     it { should allow_value(true).for(:on_tap) } 
     it { should allow_value(false).for(:on_tap) }
   end
+
+  describe "#beers_on_tap" do
+    it "returns only beer objects that are on_tap = true" do
+      brewery_1 = Brewery.create(name: "Weld Works", location: "Greeley", total_taps: 25, allow_dogs: true)
+      beer_1 = brewery_1.beers.create(style: 'IPA', on_tap: true, pint_price: 7.50, name: "Space Ghost")
+      beer_2 = brewery_1.beers.create(style: 'IPA', on_tap: true, pint_price: 7.50, name: "Juicy Bits")
+      beer_3 = brewery_1.beers.create(style: 'IPA', on_tap: false, pint_price: 6.50, name: "Willy")
+
+      expect(Beer.beers_on_tap).to eq([beer_1, beer_2])
+    end
+  end
 end
